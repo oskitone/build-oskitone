449cd61b9c358f388cdc60cf1ee0cb80685794a6
rocktronica <tommy@iamnotagoodartist.com>
2018-01-19 18:45:13 -0800

    Preview with table instead of code block
    To test React's minimal rendering. Cool!

--- components/preview.js
+++ components/preview.js
@@ -1,15 +1,45 @@
-const Preview = props => (
-    <div>
-        <pre>{JSON.stringify(props.state, null, 4)}</pre>
+import { Table } from "reactstrap";
 
-        <style jsx>{`
-            pre {
-                background: #eee;
-                color: #111;
-                padding: 1rem;
-            }
-        `}</style>
-    </div>
-);
+class Preview extends React.Component {
+    render() {
+        const vanityText = this.props.state.vanityText;
+        const keyCount = this.props.state.keyCount;
+        const startingNoteIndex = this.props.state.startingNoteIndex;
+        const color = this.props.state.color;
+
+        return (
+            <div>
+                <Table hover bordered>
+                    <thead>
+                        <tr>
+                            <td>Name</td>
+                            <td>Value</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>vanityText</th>
+                            <td>{vanityText}</td>
+                        </tr>
+                        <tr>
+                            <th>keyCount</th>
+                            <td>{keyCount}</td>
+                        </tr>
+                        <tr>
+                            <th>startingNoteIndex</th>
+                            <td>{startingNoteIndex}</td>
+                        </tr>
+                        <tr>
+                            <th>color</th>
+                            <td>{color}</td>
+                        </tr>
+                    </tbody>
+                </Table>
+
+                <style jsx>{``}</style>
+            </div>
+        );
+    }
+}
 
 export default Preview;
