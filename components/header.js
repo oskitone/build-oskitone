939833a199cf6d29f887b081a17bc72a5341935e
rocktronica <tommy@iamnotagoodartist.com>
2018-01-20 16:37:18 -0800

    Style header, I guess?

--- components/header.js
+++ components/header.js
@@ -9,6 +9,11 @@ const Header = props => (
         <style jsx>{`
             h1 {
                 margin: 1rem 0 2rem;
+                text-transform: uppercase;
+                font-weight: 800;
+                font-family: Work Sans, -apple-system, BlinkMacSystemFont,
+                    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif,
+                    "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
             }
         `}</style>
     </Row>
