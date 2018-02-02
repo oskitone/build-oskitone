0ae8e9cdb02b6037380ff02e89bbd58dcdab9cfd
rocktronica <tommy@iamnotagoodartist.com>
2018-02-02 14:14:12 -0800

    Fix deploying to github pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
+const debug = process.env.NODE_ENV !== "production";
+
 module.exports = {
     exportPathMap: function() {
         return {
             "/": { page: "/" }
         };
     },
+    assetPrefix: !debug ? "/build-oskitone/" : "",
     webpack: config => {
         // Fixes npm packages that depend on `fs` module
         config.node = {
